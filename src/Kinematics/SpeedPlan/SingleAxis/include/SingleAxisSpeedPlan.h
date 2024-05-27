48358a41faa53d62a9beea52e56b8c083dcc007b
lilongfei <yaxiaodang123@gmail.com>
2024-05-27 12:55:56 +0800

    Init Repo

---
+++ src/Kinematics/SpeedPlan/SingleAxis/include/SingleAxisSpeedPlan.h
@@ -0,0 +1,14 @@
+#include<vector>
+namespace singleaxisspeedplan{
+    class SingleAxisSpeedPlan{
+    public:
+        SingleAxisSpeedPlan(double cur_pos): m_cur_pos(cur_pos){}
+        virtual ~SingleAxisSpeedPlan(){}
+        virtual void MoveTo(double target_pos) = 0;
+    private:
+        double m_cur_pos, m_target_pos;
+        std::vector<double> m_s_list;
+
+    }
+}
+
